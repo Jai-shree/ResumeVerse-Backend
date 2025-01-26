29d411b2d112367596f2caaf0faefa334a8f68c6
Jai-shree <128470432+Jai-shree@users.noreply.github.com>
2025-01-26 22:06:53 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // Allow only your frontend
+    origin: 'https://resume-builder-five-liard.vercel.app/', // Allow only your frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Allow credentials (cookies or headers)
   });
